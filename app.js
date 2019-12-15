3dcafdb13986c29256952db808397b303a99212a
“ksagar640@gmail.com” <ksagar640@gmail.com>
2019-12-15 19:56:14 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+app.use(express.static('public'));
+app.set('view engine' , 'ejs');
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/",function(req,res){
+	res.render('home');
+});
+
+app.get('/aboutus',function(req,res){
+	res.render('aboutus');
+});
+
+app.get('/animalhusbandry',function(req,res){
+	res.render('animalhusbandry');
+});
+
+
+app.get('/animalhusbandary2',function(req,res){
+	res.render('animalhusbandry2');
+});
+
+app.get('/grading',function(req,res){
+	res.render('grading');
+});
+app.get('/aicity',function(req,res){
+	res.render('aicity');
+});
+
+app.get('/demoreq',function(req,res){
+	res.render('demoreq');
+});
+
+app.get('/contactUs',function(req,res){
+	res.render('contact');
+});
+
+app.listen(3000,function(){
+	console.log("Listening For genflow at 3000");
+});
